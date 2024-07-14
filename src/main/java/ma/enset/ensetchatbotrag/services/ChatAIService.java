6abf23c210bb89897e64154031f2331ed2ba0dff
Abdellah DAMRI <damriabdellah@gmail.com>
2024-07-14 15:44:29 +0100

    feat: first commit

---
+++ src/main/java/ma/enset/ensetchatbotrag/services/ChatAIService.java
@@ -0,0 +1,17 @@
+package ma.enset.ensetchatbotrag.services;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatAIService {
+    private ChatClient chatClient;
+
+    public ChatAIService(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    public String ragChat(String question) {
+        return chatClient.prompt().user(question).call().content();
+    }
+}
